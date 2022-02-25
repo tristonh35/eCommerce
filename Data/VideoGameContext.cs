4fa174930cf6d128be5ba1143409b695539259d6
tristonh35 <tristonhall35@yahoo.com>
2022-02-24 22:11:56 -0800

    Added Db Context class

---
+++ Data/VideoGameContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class VideoGameContext : DbContext
+    {
+        public VideoGameContext(DbContextOptions<VideoGameContext> option) : base(option)
+        {
+             
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
