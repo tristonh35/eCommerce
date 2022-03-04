5e4c211a42cb2391a99ba4e66504c3df55f564dc
tristonh35 <tristonhall35@yahoo.com>
2022-03-03 16:27:31 -0800

    Create game is fully functional with styling

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
     public class GamesController : Controller
     {
+        // Set _context as readonly so it cannot be assigned elsewhere other than
+        // a constructor
+        private readonly VideoGameContext _context;
+
+
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Game g)
+        {
+            // If everything is valid in the game class
+            // add and save changes
+            if(ModelState.IsValid)
+            {
+                _context.Games.Add(g); //Prepares insert
+                _context.SaveChanges(); //Execute pending insert
+                ViewData["Message"] = $"{g.Title} was added successfully!";
+
+                return View();
+            }
+            return View(g);
         }
     }
 }
