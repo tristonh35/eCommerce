0cde3f5de04014608c101d32696c08f94be1522a
tristonh35 <tristonhall35@yahoo.com>
2022-03-09 15:37:36 -0800

    Implemented details page

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -98,6 +98,16 @@ namespace eCommerce.Controllers
           
         }
 
+        public async Task<IActionResult> Details(int id)
+        {
+            Game? gameDetails = await _context.Games.FindAsync(id);
+
+            if (gameDetails == null)
+            {
+                return NotFound();
+            }
+            return View(gameDetails);
+        }
         
     }
 }
