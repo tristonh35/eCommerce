ceb0ce99e583dc363ff9ad2cb949b865d6c2e2d3
tristonh35 <tristonhall35@yahoo.com>
2022-03-09 17:19:12 -0800

    Basic log in page is functional

--- Models/Member.cs
+++ Models/Member.cs
@@ -43,7 +43,9 @@ namespace eCommerce.Models
     public class LoginViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; } = null!;
+
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
